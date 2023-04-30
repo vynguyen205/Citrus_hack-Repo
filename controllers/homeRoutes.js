8ffaabbb842ab26b1914aa4f2b083d5a5125dedc
Vy Nguyen <vynguyen0520@gmail.com>
2023-04-29 18:42:34 -0700

    fixed routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+//These are all the view routes for your application
+const router = require('express').Router();
+//when a GET request is received on the root(/) route,
+//render the home.handlebars view
+router.get('/', (req, res) => {
+    res.render('main');
+  });
+
+module.exports = router;
